5c810840f94e07a39bc87447a59d1e0328b8faee
sheltowt <sheltowt@gmail.com>
2016-07-09 16:26:26 -0400

    next step

---
+++ lists/lists.py
@@ -0,0 +1,11 @@
+
+#take in a list, and remove any charachters from the list 
+# if there are two charachters in the list of the same type
+def remove_more_than_two(list_charachters):
+
+
+# return True if the list is a palindrome ex. ["x", "a", "m", "a", "x"]
+# return False if the list is not a palindrome ex. ["x", "a", "m", "a", "x", "b"]
+def palindrome_checker(possible_palindrome)
+
+
