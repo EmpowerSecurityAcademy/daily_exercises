287c48905bb82f9a0d5533d0aee4a7d12d26d97c
sheltowt <sheltowt@gmail.com>
2016-07-09 16:03:11 -0400

    adding updates

---
+++ words/words.py
@@ -0,0 +1,31 @@
+#return words with a length longer than the minimum length
+def long_words(words, minimum_length):
+	return_array = []
+	for word in words:
+		if len(word) >= minimum_length:
+			return_array.append(word)
+	return return_array
+
+#return words that contain the letter a
+def a_words(words):
+
+# return a dictionary containing each unique word passed in
+# and the number of charachters in that word
+# {
+# 	"apple": 5,
+# 	"orange": 6
+# }
+def letter_count():
+
+
+pokemon = "audino bagon baltoy banette bidoof braviary bronzor carracosta charmeleon cresselia croagunk darmanitan deino emboar emolga exeggcute gabite girafarig gulpin haxorus heatmor heatran ivysaur jellicent jumpluff kangaskhan kricketune landorus ledyba loudred lumineon lunatone machamp magnezone mamoswine nosepass petilil pidgeotto pikachu pinsir poliwrath poochyena porygon porygonz registeel relicanth remoraid rufflet sableye scolipede scrafty seaking sealeo silcoon simisear snivy snorlax spoink starly tirtouga trapinch treecko tyrogue vigoroth vulpix wailord wartortle whismur wingull yamask"
+
+# write a function that takes the string pokemon
+# break it into a list of strings
+# arrange the strings so that the next word starts with a letter of the alphabet farther throught the alphabet than the previous word
+# return the longest possible list
+# example = ["bagon", "poochyena", "baltoy"]
+def longest_pokemon_list():
+
+
+
---
+++ words/words_test.py
@@ -0,0 +1,2 @@
+import unittest
+from words import long_words, a_words, letter_count, longest_pokemon_list
