35a1635d97c6a3a2e50bb3f48ad15368049ab1de
sheltowt <sheltowt@gmail.com>
2016-07-25 09:35:34 -0400

    adding updates

--- classes/customer.py
+++ classes/customer.py
@@ -2,6 +2,15 @@
 
 class Customer(object):
 
-	def __init__(self, name, skill_level="beginner")
+	def __init__(self, name, cash_available, number_of_tacos=0)
 		self.name = name
-		self.skill_level = skill_level
+		# assign cash_available to a variable of customer 
+		# called cash_available
+		# assign number_of_tacos
+
+
+	def purchase_taco(self, amount):
+		#in this function increase number_of_tacos by the amount
+		#decrease available cash by 1.5 * amount
+
+		
