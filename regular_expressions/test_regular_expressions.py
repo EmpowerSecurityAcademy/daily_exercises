25c978040c200138e9023d86e745a257ed6bacb8
sheltowt <sheltowt@gmail.com>
2016-07-25 11:58:20 -0400

    pushing updates

--- regular_expressions/test_regular_expressions.py
+++ regular_expressions/test_regular_expressions.py
@@ -1,6 +1,6 @@
 import unittest
 import re
-from regular_expressions import identify_match
+from regular_expressions import identify_match, identify_dates
 
 class TestRegularExpressions(unittest.TestCase):
 
@@ -11,8 +11,11 @@ class TestRegularExpressions(unittest.TestCase):
 		result = identify_match("cats like to wear hats", "fish")
 		self.assertEqual(result, False)
 
-	
+	def test_identify_dates(self):
+		result = identify_dates("On August 11 I will ride to Arlington, on November 15 I will write a poem")
+		self.assertEqual(result, ['August 11', 'November 15'])
 
+	
 
 if __name__ == '__main__':
 	unittest.main()
