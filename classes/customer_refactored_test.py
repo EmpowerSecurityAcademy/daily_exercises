770ba96bd2571dc7ff0980ea54824a7e0eb2e5f7
sheltowt <sheltowt@gmail.com>
2016-07-25 10:57:53 -0400

    pushing refactored code

--- classes/customer_refactored_test.py
+++ classes/customer_refactored_test.py
@@ -22,8 +22,34 @@ class TestCustomerRefactored(unittest.TestCase):
 		self.assertEqual(test_customer.prices, prices)
 		self.assertEqual(test_customer.food, {})
 
-	
+	def test_calculate_purchase_amount(self):
+		order = {
+			"taco": 5,
+			"burrito": 3,
+			"enchilladas": 2
+		}
+		test_customer = CustomerRefactored("bill shelton", 150, prices)
+		cost = test_customer(order)
+		self.assertEqual(cost, 46)
 
+	def test_purchase_food(self):
+		order = {
+			"taco": 5,
+			"burrito": 3,
+			"enchilladas": 2
+		}
+		test_customer = CustomerRefactored("bill shelton", 150, prices)
+		self.assertEqual(test_customer.cash_available, 104)
+		self.assertEqual(test_customer.food, order)
 
+	def test_eat_food(self):
+		order = {
+			"taco": 7,
+			"burrito": 4,
+			"enchilladas": 2
+		}
+		test_customer = CustomerRefactored("bill shelton", 150, prices)
+		
+		
 if __name__ == '__main__':
 	unittest.main()
