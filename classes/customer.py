d795f7eeaa57a86f97d1f0bac0f86bed228fa158
sheltowt <sheltowt@gmail.com>
2016-07-25 09:46:46 -0400

    adding updates

--- classes/customer.py
+++ classes/customer.py
@@ -2,15 +2,18 @@
 
 class Customer(object):
 
-	def __init__(self, name, cash_available, number_of_tacos=0)
+	def __init__(self, name, cash_available, number_of_tacos=0):
 		self.name = name
-		# assign cash_available to a variable of customer 
-		# called cash_available
-		# assign number_of_tacos
+		# assign cash_available to a variable of customer called cash_available
+		# assign number_of_tacos to a variable of customer called number_of_tacos
+		self.cash_available = cash_available
+		self.number_of_tacos = number_of_tacos
 
 
-	def purchase_taco(self, amount):
+	def purchase_tacos(self, amount):
 		#in this function increase number_of_tacos by the amount
 		#decrease available cash by 1.5 * amount
+		self.number_of_tacos += amount
+		self.cash_available -= (1.5 * amount)
 
-		
+
--- classes/customer_test.py
+++ classes/customer_test.py
@@ -1,7 +1,21 @@
 import unittest
-from words import long_words, a_words, letter_count, longest_pokemon_list
+from customer import Customer
 
-class TestWords(unittest.TestCase):
+class TestCustomer(unittest.TestCase):
+
+	def test_init(self):
+		test_customer = Customer("bill shelton", 5)
+
+		self.assertEqual(test_customer.cash_available, 5)
+		self.assertEqual(test_customer.number_of_tacos, 0)
+
+	def test_purchase_tacos(self):
+		test_customer = Customer("bill shelton", 5)
+		test_customer.purchase_tacos(2)
+
+		self.assertEqual(test_customer.cash_available, 2)
+		self.assertEqual(test_customer.number_of_tacos, 2)
+
 
 if __name__ == '__main__':
 	unittest.main()
