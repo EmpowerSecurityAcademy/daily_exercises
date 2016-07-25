36ed6c923a83e5cd1b6556a98fb29456f3c5554a
sheltowt <sheltowt@gmail.com>
2016-07-25 13:19:30 -0400

    lambda operator

--- list_comprehensions/list_comprehensions.py
+++ list_comprehensions/list_comprehensions.py
@@ -10,6 +10,9 @@ def even_numbers(array_numbers):
 def start_with_a(array_strings):
 	return [s_str for s_str in array_strings if s_str[0] == 'a']
 
+# use list comprehension to multiply by 11 numbers that are divisable by 3
 def multiply_by_11_numbers_divisable_by_three(array_numbers):
 	return [num*11 for num in array_numbers if num % 3 == 0]
+
+
 
