3ebcd71cdd0cca928acd3c93339d20ab57ed8499
sheltowt <sheltowt@gmail.com>
2016-07-25 10:42:22 -0400

    adding refactored customer

--- classes/customer_refactored.py
+++ classes/customer_refactored.py
@@ -15,9 +15,27 @@ class CustomerRefactored(object):
 		self.food = {}
 
 	def calculate_purchase_amount(self, order):
-		#the order object 
+		#the order object is a dictionary, with food types and amounts
+		# {
+		# 	"burritos": 1,
+		# 	"tacos": 3,
+		# 	"enchiladas" 1
+		# }
+		# this function uses self.prices to calculate the cost of all the food in an order
+		# and then returns that amount
+		bill_amount = 0 
 
+		for food_type, amount in order:
+			bill_amount += self.prices["food_type"] * amount
+
+		return bill_amount
 
 	def purchase_food(self, order):
+		self.cash_available -= self.calculate_purchase_amount(order)
+		for food_type, amount in order:
+			self.food["food_type"] += amount
+
 
 	def eat_food(self, food_eaten):
+		for food_type, amount in food_eaten:
+			self.food["food_type"] -= amount
