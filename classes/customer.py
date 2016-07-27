a1373f2ee4248c85d13bf24149d79b6f47eff26c
RickDGarcia <rick@empowersecurityacademy.com>
2016-07-27 15:22:38 -0400

    completed the customer and customer_refactored python exercises and uploaded the to the rickapod branch

--- classes/customer.py
+++ classes/customer.py
@@ -6,27 +6,36 @@ class Customer(object):
 
 	def __init__(self, name, cash_available, number_of_tacos=0, number_of_burritos=0):
 		self.name = name
+		self.cash_available = cash_available
+		self.number_of_tacos = number_of_tacos
+		self.number_of_burritos = number_of_burritos
 		# assign cash_available to a variable of customer called cash_available
 		# assign number_of_tacos to a variable of customer called number_of_tacos
 		# assign number_of_burritos to a variable of custmer called number_of_burritos
 
 
-	# def purchase_tacos(self, number_tacos_purchased):
+	def purchase_tacos(self, number_tacos_purchased):
 	# 	#in this function increase number_of_tacos by the amount
 	# 	#decrease available cash by 1.5 * amount
+		self.number_of_tacos += number_tacos_purchased
+		self.cash_available -= number_tacos_purchased *1.5
 
 
-	# def eat_tacos(self, number_tacos_eaten):
+
+	def eat_tacos(self, number_tacos_eaten):
 	# 	#subtract the number of tacos eaten from self.number_of_tacos
+		self.number_of_tacos -= number_tacos_eaten
 
-	# def purchase_burritos(self, number_burritos_purchased):
+
+	def purchase_burritos(self, number_burritos_purchased):
 	# 	#in this function increase number_of_burritos by the amount
 	# 	#decrease available cash by 7 * amount		
+		self.number_of_burritos += number_burritos_purchased
+		self.cash_available -= 7*number_burritos_purchased
 
-	# def eat_burritos(self, number_burritos_eaten):
+	def eat_burritos(self, number_burritos_eaten):
 	# 	#subtract the number of tacos eaten from self.number_of_tacos
-
-
+		self.number_of_burritos -= number_burritos_eaten
 
 
 
--- classes/customer_refactored.py
+++ classes/customer_refactored.py
@@ -8,27 +8,45 @@ class CustomerRefactored(object):
 
 	def __init__(self, name, cash_available, prices, food={}):
 		self.name = name
+		self.cash_available = cash_available
+		self.prices = prices
+		self.food = {}
 		# assign cash_available to a variable of customer called cash_available
 		# assign prices to a variable of customer named prices
 		# assign food to a variable of custmer named food
 
 
 	def calculate_purchase_amount(self, order):
-		#the order object is a dictionary, with food types and amounts
-		# {
-		# 	"burritos": 1,
-		# 	"tacos": 3,
-		# 	"enchiladas" 1
-		# }
-		# this function uses self.prices to calculate the cost of all the food in an order
-		# and then returns that amount
+	#the order object is a dictionary, with food types and amounts
+	# order = {
+	# 	"burritos": 1,
+	# 	"tacos": 3,
+	# 	"enchiladas": 1}
+	# this function uses self.prices to calculate the cost of all the food in an order
+	# and then returns that amount
+		purchase_amount = 0
+		for food_item, amount in order.items():
+			purchase_amount += self.prices[food_item] * amount
+
+		return purchase_amount
 
 
 	def purchase_food(self, order):
-		# use calculate_purchase_amount to calculate the cost of food
-		# remove the purchase price of food from self.cash_available
-		# add food ordered to the self.food variable
+	# use calculate_purchase_amount to calculate the cost of food
+	# remove the purchase price of food from self.cash_available
+	# add food ordered to the self.food variable
+		self.cash_available -= self.calculate_purchase_amount(order)
+		for food_item, amount in order.items():
+			if food_item in self.food:
+				self.food[food_item] += amount
+			else:
+				self.food[food_item] = amount
 
 
 	def eat_food(self, food_eaten):
-		#remove from self.food the food in the food_eaten object 
+	#remove from self.food the food in the food_eaten object
+		for food_item, amount in food_eaten.items():
+			if food_item in self.food:
+				self.food[food_item] -= food_eaten[food_item]
+				# del self.food[food_items]
+			
