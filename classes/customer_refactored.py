fd622f689a156fab6494b11a29618603d64a6265
Steven VanWoerkom <stevenvanwoerkom@Stevens-MacBook-Pro.local>
2016-07-27 15:23:04 -0400

    finale

--- classes/customer_refactored.py
+++ classes/customer_refactored.py
@@ -10,7 +10,10 @@ class CustomerRefactored(object):
 		self.name = name
 		# assign cash_available to a variable of customer called cash_available
 		# assign prices to a variable of customer named prices
-		# assign food to a variable of custmer named food
+		# assign food to a variable of customer named food
+		self.cash_available = cash_available
+		self.prices = prices
+		self.food = {}
 
 
 	def calculate_purchase_amount(self, order):
@@ -22,13 +25,30 @@ class CustomerRefactored(object):
 		# }
 		# this function uses self.prices to calculate the cost of all the food in an order
 		# and then returns that amount
+		purchase_amount = 0
 
+		for food_item, amount in order.iteritems():
+			purchase_amount += self.prices[food_item] * amount
+			
+		return purchase_amount
 
 	def purchase_food(self, order):
 		# use calculate_purchase_amount to calculate the cost of food
 		# remove the purchase price of food from self.cash_available
 		# add food ordered to the self.food variable
-
+		self.cash_available -= self.calculate_purchase_amount(order)
+		for food_item, amount in order.iteritems():
+			if food_item in self.food:
+				self.food[food_item] += amount 
+			else:
+				self.food[food_item] = amount	
+	
 
 	def eat_food(self, food_eaten):
 		#remove from self.food the food in the food_eaten object 
+		for food_item, amount in food_eaten.iteritems():
+			if food_item in self.food:
+				self.food[food_item] -= food_eaten[food_item]
+			else:
+				self.food[food_item] = food_eaten[food_item]
+
