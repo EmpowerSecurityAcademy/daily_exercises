eeaff0ab42609960813cc7e631f0263b01f529fc
sheltowt <sheltowt@gmail.com>
2016-07-29 12:26:57 -0400

    adding updates to test functions

--- numbers/numbers.py
+++ numbers/numbers.py
@@ -2,10 +2,16 @@
 # write a function that sums all multiples of 3 and 5
 # below a certain number
 def sum_3_5_multiples(ceiling_number):
+	total = 0
+	for num in range(ceiling_number):
+		if num % 3 or num % 5:
+			total += num
+	return total
 
 
 #find the sum of all fibonacci numbers below 1,000,000
 def sum_fibonacci_numbers_below_1000000():
+	
 
 
 
@@ -13,4 +19,4 @@ def sum_fibonacci_numbers_below_1000000():
 #palindrome number is the same forward and backwards
 #find the largest palindrome number that is the product of two two digit numbers
 #example 9009 = 91 * 99
-def largest_palindrome_of_thee_digit_numbers():
+#def largest_palindrome_of_thee_digit_numbers():
--- numbers/numbers_test.py
+++ numbers/numbers_test.py
@@ -1,14 +1,22 @@
 import unittest
-from numbers import 
+from numbers import *
 
 class TestNumbers(unittest.TestCase):
 
 	def test_sum_3_5_multiples(self):
+		result = sum_3_5_multiples(155)
+		self.assertEqual(result, 11110)
+
 
 	def test_sum_fibonacci_numbers_below_1000000(self):
 
-	def test_largest_palindrome_of_thee_digit_numbers():
+
+
+
+	# def test_largest_palindrome_of_thee_digit_numbers(self):
 		
+
+
 
 if __name__ == '__main__':
 	unittest.main()
