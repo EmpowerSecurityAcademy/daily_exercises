19af2fd336e1f3fdfdf28cdb8e5e8ceb36a0c471
sheltowt <sheltowt@gmail.com>
2016-08-04 10:25:14 -0400

    adding updates

--- dictionaries/dictionaries.py
+++ dictionaries/dictionaries.py
@@ -12,7 +12,6 @@ def construct_dictionary(dictionary_list):
 	return_value = {}
 
 	for element in dictionary_list:
-		print(element)
 		for key, value in element.items():
 			return_value[key] = value
 
@@ -30,3 +29,35 @@ def unpack_dictionary(packed_dictionary):
 		return_value.append(dict_element)
 
 	return return_value
+
+# in this function we will pass in two dictionaries
+# we will look up the keys associated with the first dictionary in the second dictionary
+# we will add each value associated with the key in the second dictionary into a list
+# we will return the list of values 
+def lookup_values(first_dictionary, second_dictionary):
+	return_value = []
+
+	for key in first_dictionary:
+		return_value.append(second_dictionary[key])
+
+	return return_value
+
+
+# in this function we will pass in two dictionaries, and a color
+# we will look up the keys associated with the first dictionary in the second dictionary
+# if the key in the first dictionary has a value matching the value being passed in
+# we will add each value associated with the key in the second dictionary into a list
+# we will return the list of values 
+def lookup_values_by_color(first_dictionary, second_dictionary, color):
+	return_value = []
+
+	for key, value in first_dictionary.items():
+		if value == color:
+			return_value.append(second_dictionary[key])
+
+	return return_value
+
+
+
+
+
