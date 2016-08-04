77626166514ffb655336240bdd421c2effa21c58
sheltowt <sheltowt@gmail.com>
2016-08-04 12:48:03 -0400

    adding updates

--- sets/sets.py
+++ sets/sets.py
@@ -1,6 +1,11 @@
 # create a set
 # set("my name is Eric and Eric is my name".split())
 
+# use .split(" ") to split the input, and return it as a set
+def create_set_from_sentence(sentence):
+	split_sentence = sentence.split(" ")
+	return set(split_sentence)
+
 # find the interection of two sets
 # a.intersection(b)
 
--- sets/sets_test.py
+++ sets/sets_test.py
@@ -3,10 +3,10 @@ from sets import *
 
 class TestSets(unittest.TestCase):
 
-	def test_create_list_of_range(self):
+	def test_create_set_from_sentence(self):
 
-		result = create_list_of_range(3, 99)
-		self.assertEqual(result, [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99])
+		result = create_set_from_sentence("driving in the District can be a struggle driving in new york and driving in san francisco can be a struggle as well")
+		self.assertEqual(result, set(['a', 'be', 'and', 'francisco', 'driving', 'District', 'struggle', 'well', 'as', 'new', 'york', 'can', 'in', 'san', 'the']))
 
 
 
