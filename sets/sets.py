53f29a892ef3051a746c5ecf9681f8e05637b014
sheltowt <sheltowt@gmail.com>
2016-08-04 12:06:33 -0400

    launching off on sets

---
+++ sets/sets.py
@@ -0,0 +1,14 @@
+# create a set
+# set("my name is Eric and Eric is my name".split())
+
+# find the interection of two sets
+# a.intersection(b)
+
+# find elements that are unique to one of the lists
+# a.symmetric_difference(b)
+
+# find all elements unique to a set
+# a.difference(b)
+
+# return one of each elements
+# a.union(b)
