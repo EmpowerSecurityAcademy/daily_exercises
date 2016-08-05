1f1193d8fce19370910f900317a9fd06e4a54b29
sheltowt <sheltowt@gmail.com>
2016-08-05 09:25:47 -0400

    adding updates

---
+++ tuples/tuples.py
@@ -0,0 +1,8 @@
+# create a set
+
+# use .split(" ") to split the input, and return it as a set
+def create_set_from_sentence(sentence):
+	split_sentence = sentence.split(" ")
+	return set(split_sentence)
+
+
---
+++ tuples/tuples_test.py
@@ -0,0 +1,12 @@
+import unittest
+from tuples import * 
+
+class TestTuples(unittest.TestCase):
+
+	def test_create_set_from_sentence(self):
+
+		result = create_set_from_sentence("driving in the District can be a struggle driving in new york and driving in san francisco can be a struggle as well")
+		self.assertEqual(result, set(['a', 'be', 'and', 'francisco', 'driving', 'District', 'struggle', 'well', 'as', 'new', 'york', 'can', 'in', 'san', 'the']))
+
+if __name__ == '__main__':
+	unittest.main()
